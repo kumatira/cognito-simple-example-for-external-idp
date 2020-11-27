a637740ed2233e1a179c92b10b7aaa9f7215a988
kumatira <kumano@val.co.jp>
2020-11-27 22:32:29 +0900

    認証結果を見るページを追加

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static('public'));
+app.use(express.static(__dirname + '/public'));
 
 app.set("view engine", "ejs");
 
 app.get('/', (req, res) => {
     res.render('index');
 });
+
+app.get('/auth/index', (req, res) => {
+    res.render('auth');
+});
+
 
 const server = app.listen(3000, function () {
     console.log("Node.js is listening to PORT:" + server.address().port);
